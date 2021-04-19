a7af917c71272f24820770ae2e6b78cba669e8fb
kardash-bp <kardasbp@gmail.com>
2021-04-19 11:01:22 +0200

    start

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import fs from 'fs'
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','))
+console.log(matches)
